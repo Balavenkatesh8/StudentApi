7766be1076ff44afd581f0653eca2d0040e268c7
Balavenkatesh8 <bala.narnepati@rubissoftwaresolutions.com>
2026-03-18 19:07:37 +0530

    Update RegisterDto.cs

--- DTO/RegisterDto.cs
+++ DTO/RegisterDto.cs
@@ -8,18 +8,11 @@
 
         public string Password { get; set; } = string.Empty;
         public long Phoneno { get; set; }
+        public string IsActive {get: set;} = string .Empty;
 
-        public string RoleName { get; set; } = string.Empty;
+        //public string RoleName { get; set; } = string.Empty;
         public string Role { get; set; }
     }
 
 }
 
-//public class RegisterDto
-//{
-//    public string? Username { get; set; } = string.Empty;
-//    public string? Email { get; set; } = string.Empty;
-//    public string? Password { get; set; } = string.Empty;
-//    public long Phoneno { get; set; }
-//    public string? RoleName { get; set; }
-//}
